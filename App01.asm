7b1a2f1ca95392988a4c5d5a84ac6303ee7d2f49
Christopher Meléndez <christophermelendez13@gmail.com>
2020-03-25 17:35:43 -0700

    Hola mundo
    Hola mundo hecho en lenguaje ensamblador.

---
+++ App01.asm
@@ -0,0 +1,15 @@
+;Hola mundo
+            .MODEL small
+            .STACK
+            .DATA
+mensaje     DB "Hola, mundo!$" ;el signo $ indica hasta d?nde es el fin del mensaje, si no se coloca imprime algo muy raro
+            .CODE
+inicio      proc            ;iniciar proceso con proc
+            .STARTUP
+            mov     bx, 0001H
+            lea     dx, mensaje
+            mov     ah, 09H  ;llama a imprimir
+            int     21H
+            .EXIT
+inicio      endp
+            end
