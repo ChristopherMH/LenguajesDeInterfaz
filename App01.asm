c6210b762ec081cea357acd43fafd2952848df7e
Christopher Meléndez <christophermelendez13@gmail.com>
2020-04-15 21:04:16 -0600

    Hola Mundo

--- App01.asm
+++ App01.asm
@@ -1,15 +1,14 @@
 ;Hola mundo
-            .MODEL small
-            .STACK
-            .DATA
-mensaje     DB "Hola, mundo!$" ;el signo $ indica hasta donde es el fin del mensaje, si no se coloca imprime algo muy raro
-            .CODE
-inicio      proc            ;iniciar proceso con proc
-            .STARTUP
-            mov     bx, 0001H
-            lea     dx, mensaje
-            mov     ah, 09H  ;llama a imprimir
-            int     21H
-            .EXIT
-inicio      endp
+            .model small ;establecer el tamanio del programa
+            .stack
+            .data       
+mensaje     db "Hola, mundo!$" ;el signo $ indica hasta donde es el fin del mensaje, si no se coloca imprime algo muy raro
+            .code
+hola        proc            ;iniciar proceso con proc
+            .startup
+            mov     ah, 09h ;impresion de cadena
+            lea     dx, mensaje ;el mensaje se manda al registro dx (de datos) para mostrar el mensaje en la pantalla
+            int     21h;interrupci?n
+            .exit
+hola        endp
             end
